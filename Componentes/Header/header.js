067f0588c84a9f244569193db1a87324fd77db8c
Samuel <sajeronimo@scl.edu.gt>
2025-02-04 13:17:09 -0600

    Modularizacion -Se cream modulos 	-Header 	-tarea 	-formulario

---
+++ Componentes/Header/header.js
@@ -0,0 +1,8 @@
+function header() {
+    let header = document.createElement('header');
+    header.className = "div-header";
+
+    return header;
+}
+
+export { header };
--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-alert("hola mundo");
+import { header } from "./Componentes/Header/header.js";
+import { formulario } from "./Componentes/Formulario/formulario.js";
+import { tarea } from "./Componentes/Tarea/tarea.js";
+
+let DOM = document.getElementById('root');
+
+DOM.appendChild(header());
+DOM.appendChild(formulario());
+DOM.appendChild(tarea());
