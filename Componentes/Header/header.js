a5ad55f4e887ed95dfc077c8eda718b696b05881
Samuel <sajeronimo@scl.edu.gt>
2025-02-04 16:22:50 -0600

    Se creo el Header y agrego diseño

--- Componentes/Header/header.js
+++ Componentes/Header/header.js
@@ -2,6 +2,16 @@ function header() {
     let header = document.createElement('header');
     header.className = "div-header";
 
+    //contenido
+    let logo = document.createElement('img');
+    logo.src = "https://cdn-icons-png.flaticon.com/512/562/562153.png";
+    logo.alt = "logo";
+    header.appendChild(logo);
+
+    let h1 = document.createElement('h1');
+    h1.textContent = "Todo-List";
+    header.appendChild(h1);
+
     return header;
 }
 
