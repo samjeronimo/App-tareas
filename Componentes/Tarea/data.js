55e9f8b416337127be2faaf718d8a155a4f7b73a
Samuel <sajeronimo@scl.edu.gt>
2025-02-14 19:48:16 -0600

    Se finaliza el diseño y se avanza con la funcion para pasar lista o entregar tareas 	-index.js 	-tarea.js 	-tarea.css Y se crea un nuevo archivo para poder crear una lista o más bien elementos 	-data.js

---
+++ Componentes/Tarea/data.js
@@ -0,0 +1,14 @@
+let tareas = [
+    "Drink 8 glasses of water",
+    "Meditate for 10 minutes",
+    "Read a chapter of a book",
+    "Go for a 30-minute walk",
+    "Write in a gratitude journal",
+    "Plan meals for the day",
+    "Practice deep breathing exercises",
+    "Stretch for 15 minutes",
+    "Limit screen time before bed",
+  ];
+  
+  export { tareas };
+  
